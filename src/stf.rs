c4287a2d6340ff718e0b3a41b43e6dbb4485693e
Nikolai Golub <nikolay.v.golub@gmail.com>
2023-10-02 13:41:13 +0200

    Added simple STF placeholder

--- src/stf.rs
+++ src/stf.rs
@@ -1,11 +1,53 @@
-use crate::state::State;
+use std::collections::HashMap;
+use crate::Operation;
 
-/// Process batch of operations and return new snapshot
-fn stf<S: State>(base_snapshot: S, operations_count: usize) -> S {
-    for _batch_id in 0..3 {
-        for _i in 0..operations_count {
-            // DO SOMETHING
+
+pub trait STF {
+    type StateRoot;
+    type Witness;
+    type BlobTransaction;
+
+
+    fn apply_slot<'a, I>(
+        &mut self,
+        pre_state_root: &Self::StateRoot,
+        blobs: I,
+    ) ->
+        (Self::StateRoot,
+         Self::Witness)
+        where
+            I: IntoIterator<Item=Self::BlobTransaction>;
+}
+
+
+struct SampleSTF {
+    state_root: u64,
+    state: HashMap<String, u64>,
+}
+
+impl STF for SampleSTF {
+    type StateRoot = u64;
+    type Witness = Vec<u64>;
+    type BlobTransaction = Operation;
+
+    fn apply_slot<'a, I>(&mut self, pre_state_root: &Self::StateRoot, blobs: I) -> (Self::StateRoot, Self::Witness) where I: IntoIterator<Item=Self::BlobTransaction> {
+        let mut prev_state_root = self.state_root;
+        for operation in blobs {
+            match operation {
+                Operation::Get(key) => {
+                    println!("Get {}", key);
+                    prev_state_root += 1;
+                }
+                Operation::Set(key, value) => {
+                    println!("Set {} = {}", key, value);
+                    prev_state_root += 3;
+                }
+                Operation::Delete(delete) => {
+                    println!("Delete {}", delete);
+                    prev_state_root += 3
+                }
+            }
         }
+        (prev_state_root, vec![prev_state_root])
     }
-    base_snapshot
 }
